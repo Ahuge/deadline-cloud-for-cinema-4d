6cc92061773d471e748b9907c59e4fa60679fe37
Alex Hughes <ahughesalex@gmail.com>
2024-06-14 13:35:02 -0700

    Update our adaptor script to cd to the setup dir

--- src/deadline/cinema4d_adaptor/Cinema4DAdaptor/adaptor.sh
+++ src/deadline/cinema4d_adaptor/Cinema4DAdaptor/adaptor.sh
@@ -10,6 +10,7 @@ echo "  Passed argument list: ${ARGS}"
 C4DBASE=$(dirname "${C4DEXE}")
 if [ -f "${C4DBASE}/setup_c4d_env" ]
 then
+  cd $C4DBASE
   echo "Sourcing setup_c4d_env from ${C4DBASE}/setup_c4d_env";
   source "${C4DBASE}/setup_c4d_env";
 else
