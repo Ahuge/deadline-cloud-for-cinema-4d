cd72fc7bff87eed5e33145d54c5a9ebfeca3ceff
Alex Hughes <ahughesalex@gmail.com>
2024-06-14 12:47:39 -0700

    Fixup syntax

--- src/deadline/cinema4d_adaptor/Cinema4DAdaptor/adaptor.sh
+++ src/deadline/cinema4d_adaptor/Cinema4DAdaptor/adaptor.sh
@@ -8,11 +8,12 @@ echo "  Passed argument list: ${ARGS}"
 
 
 C4DBASE=$(dirname "${C4DEXE}")
-if [ -f "${C4DBASE}/setup_c4d_env" ];
-  echo "Sourcing setup_c4d_env from ${C4DBASE}/setup_c4d_env"
-  source "${C4DBASE}/setup_c4d_env"
+if [ -f "${C4DBASE}/setup_c4d_env" ]
+then
+  echo "Sourcing setup_c4d_env from ${C4DBASE}/setup_c4d_env";
+  source "${C4DBASE}/setup_c4d_env";
 else
-  echo "setup_c4d_env not found in ${C4DBASE}"
+  echo "setup_c4d_env not found in ${C4DBASE}";
 fi
 
 echo "Executing C4D Executable with argument list after sourcing environment"
